9dc0c20949b6823c60017a324b13b5ff6a8f1269
Shashwatt699 <Shashwatsaxena4545@gmail.com>
2025-08-30 00:18:03 +0530

    Completed just has to deploy

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,11 @@ import Navbar from "./components/Navbar"
 import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowCaseSection from "./sections/ShowCaseSection"
+import ExperienceSection from "./sections/ExperienceSection"
+import TechStack from "./sections/TechStack"
+import Testimonials from "./sections/Testimonials"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
 
 const App = () => {
   return (
@@ -12,6 +17,11 @@ const App = () => {
         <ShowCaseSection />
         <LogoSection />
         <FeatureCards />
+        <ExperienceSection />
+        <TechStack />
+        <Testimonials />
+        <Contact />
+        <Footer />
     </>
   )
 }
